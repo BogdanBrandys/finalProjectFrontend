81599d474dc9a41c657fc15672bd02b94b9b14ae
Bogdan Brandys <brandpol@poczta.onet.pl>
2024-11-19 21:36:07 +0100

    first commit

---
+++ src/main/java/com/kodilla/final_project_frontend/vaadin/BookType.java
@@ -0,0 +1,5 @@
+package com.kodilla.final_project_frontend.vaadin;
+
+public enum BookType {
+    IT, THRILLER, CLASSIC, POETRY
+}
---
+++ src/test/java/com/kodilla/final_project_frontend/FinalProjectFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.final_project_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
